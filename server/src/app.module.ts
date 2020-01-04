f7fecd4bc58fa11999f9367122e5fa3325481e9b
Alfxjx <xjxtju@163.com>
2020-01-04 16:55:09 +0800

    change port

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,7 +6,7 @@ import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest-blog', {
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-blog', {
       useNewUrlParser: true,
     }),
     BlogModule,
