5e1ab62b2014a9804e765a7d934bba5bf5317f04
AlfredPC <xjxtju@163.com>
2020-02-12 22:01:31 +0800

    init vuepress

---
+++ docs/docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Blog of Blog',
+  description: 'personal full stack blog',
+  themeConfig: {
+    lastUpdated: 'Last Updated', 
+    sidebar: [
+      '/',
+      '/note/client.md',
+      '/note/server.md'
+    ]
+  }
+}
---
+++ docs/docs/README.md
@@ -0,0 +1,9 @@
+---
+home: true
+heroImage: /hero.png
+heroText: Abandon.work
+tagline: 我的博客的开发记录博客
+actionText: 查看笔记 →
+actionLink: /note/client/
+footer: MIT Licensed | Copyright © 2019-2020 Alfxjx
+---
