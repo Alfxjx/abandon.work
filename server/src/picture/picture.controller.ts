5f857e1e1bc3a741404877eb0aa7ca4a9d95230a
alfxjx <xjxtju@163.com>
2020-08-20 17:46:55 +0800

    picture server

--- server/src/picture/picture.controller.ts
+++ server/src/picture/picture.controller.ts
@@ -7,31 +7,47 @@ import {
   HttpStatus,
   Body,
   UseInterceptors,
+  Get,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { createWriteStream } from 'fs';
 import { join } from 'path';
+import { PictureService } from "./picture.service";
 
 @Controller('picture')
 export class PictureController {
-  constructor() {}
+  constructor(private pictureService: PictureService) { }
 
   @Post('')
   @UseInterceptors(FileInterceptor('file'))
   async uploadSinglePicture(@Res() res, @UploadedFile() file) {
     console.log(file.originalname);
-    let img = createWriteStream(
-      join(__dirname, '../..', 'public', `${file.originalname}`),
-    );
-    img.write(file.buffer);
     if (!file) throw new BadRequestException('400 use pic');
     let size: number = Math.floor(file.size / 1024);
+    let img = await createWriteStream(
+      join(__dirname, '../..', 'public', `${file.originalname}`),
+    );
+    await img.write(file.buffer);
+    await this.pictureService.addPicture({
+      filename: file.originalname,
+      url: `http://www.abandon.work/api/public/${file.originalname}`,
+      size: `${size}KB`,
+    })
     return res.status(HttpStatus.OK).json({
       status: HttpStatus.OK,
       message: 'Picture has been submitted successfully!',
       data: file.originalname,
-      link: `http://abandon.work/api/public/${file.originalname}`,
+      link: `http://www.abandon.work/api/public/${file.originalname}`,
       size: `${size}KB`,
     });
+  }
+
+  @Get()
+  async getPictureList(@Res() res) {
+    let list = await this.pictureService.findAll();
+    return res.status(HttpStatus.OK).json({
+      status: HttpStatus.OK,
+      data: list
+    })
   }
 }
--- server/src/picture/picture.module.ts
+++ server/src/picture/picture.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import {PictureController} from './picture.controller'
+import { PictureController } from './picture.controller'
+import { PictureService } from './picture.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PictureSchema } from './schemas/picture.schema';
 
 @Module({
-  providers: [],
+  imports: [MongooseModule.forFeature([{ name: 'Picture', schema: PictureSchema }])],
+  providers: [PictureService],
   controllers: [PictureController],
 })
-export class PictureModule {}
+export class PictureModule { }
