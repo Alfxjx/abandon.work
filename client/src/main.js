fffc73107e2d0a0116a3e65fe060f7eafca35562
xujianxiang <xjxtju@163.com>
2020-11-06 10:40:14 +0800

    fix：directives folder

--- client/src/main.js
+++ client/src/main.js
@@ -5,26 +5,17 @@ import store from "./store";
 import { Loading } from "./plugins/index";
 import "./styles/reset.css";
 import vuetify from "./plugins/vuetify";
-import hljs from "highlight.js";
-import 'highlight.js/styles/github.css';
-// import "highlight.js/styles/atom-one-dark-reasonable.css";
+import "./plugins/directives/hljs";
 
 Vue.config.productionTip = false;
 
 Vue.use(Loading, {
-  hint: "loading...",
-});
-
-Vue.directive("highlight", (el) => {
-  let blocks = el.querySelectorAll("pre code");
-  blocks.forEach((block) => {
-    hljs.highlightBlock(block);
-  });
+	hint: "loading...",
 });
 
 new Vue({
-  vuetify,
-  router,
-  store,
-  render: (h) => h(App),
+	vuetify,
+	router,
+	store,
+	render: (h) => h(App),
 }).$mount("#app");
