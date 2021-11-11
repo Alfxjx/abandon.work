96af94ea329b214330bd7225f80c762038192239
xujx <xjxtju@163.com>
2021-11-11 21:28:36 +0800

    合规

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,44 +1,47 @@
 <template>
-    <div id="app">
+	<div id="app">
 		<the-header></the-header>
-        <router-view></router-view>
-        <scroll-top class="top"></scroll-top>
-    </div>
+		<router-view></router-view>
+		<scroll-top class="top"></scroll-top>
+		<beian></beian>
+	</div>
 </template>
 
 <script>
 import ScrollTop from "./components/scroll-top.vue";
 import TheHeader from "./components/TheHeader.vue";
+import beian from "./components/Beian.vue";
 
 export default {
-    components: {
-        "scroll-top": ScrollTop,
-		"the-header": TheHeader
-    },
+	components: {
+		"scroll-top": ScrollTop,
+		"the-header": TheHeader,
+		beian: beian,
+	},
 };
 </script>
 
 <style lang="scss">
 @import "./styles/common.scss";
 #app {
-    display: flex;
-    position: relative;
-    flex-direction: column;
-    height: 100vh;
-    text-rendering: geometricPrecision;
-    .top {
-        position: fixed;
-        bottom: 1rem;
-        right: 1.5rem;
-    }
+	display: flex;
+	position: relative;
+	flex-direction: column;
+	height: 100vh;
+	text-rendering: geometricPrecision;
+	.top {
+		position: fixed;
+		bottom: 1rem;
+		right: 1.5rem;
+	}
 }
 code {
-    background-color: #f8f8f8 !important;
-    color: #333333 !important;
-    padding: 0 0.5rem !important;
+	background-color: #f8f8f8 !important;
+	color: #333333 !important;
+	padding: 0 0.5rem !important;
 }
 kbd {
-    font-weight: 400 !important;
-    font-size: 100% !important;
+	font-weight: 400 !important;
+	font-size: 100% !important;
 }
 </style>
---
+++ client/src/components/Beian.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer">
+    <a href="https://beian.miit.gov.cn/">津ICP备18010186号-2</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
